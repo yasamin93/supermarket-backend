72bed5db01bc2b445e7096be4e116df231ca920b
YEslahi <yasi.e72@gmail.com>
2021-06-06 18:58:23 +0200

    add - registration url in main project

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('api/v1/', include('products.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/dj-rest-auth/registration/',  # new
+         include('dj_rest_auth.registration.urls')),
 ]
